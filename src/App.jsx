441f6e41372e4b2cc36c1ae653f9768ea7767fa5
imravipansuriya <dobariyasakshi512.com>
2025-07-10 12:01:02 +0530

    Bar page design

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import Shop from './pages/Shop'
 import Home from './pages/Home'
+import Bar from './pages/Bar'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/bar" element={<Bar />} />
       </Route>
     </Routes>
   )
